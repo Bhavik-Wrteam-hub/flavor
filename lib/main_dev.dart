28abb7c8a350ce888f3ab9d31b7ed5ec34cfadbd
Bhavik Pindoriya <wrteam.bhavik@gmail.com>
2024-08-27 18:09:22 +0530

    init

---
+++ lib/main_dev.dart
@@ -0,0 +1,9 @@
+import 'package:flavor_example/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_flavor/flutter_flavor.dart';
+
+void main() {
+    return runApp(MyApp(
+      flavor: "Devlopment",
+    ));
+}
---
+++ lib/main_prod.dart
@@ -0,0 +1,8 @@
+import 'package:flavor_example/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(const MyApp(
+    flavor: "Production",
+  ));
+}
